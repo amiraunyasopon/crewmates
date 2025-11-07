f7c9a92d8622a4e1fb4ddc10394801fb6c2be1ba
amiraunyasopon <amiraunyasopon@gmail.com>
2025-11-07 15:37:11 -0800

    display functionality

--- src/components/Crewmate.jsx
+++ src/components/Crewmate.jsx
@@ -1,10 +1,15 @@
 import "./Crewmate.css"
+import { Link } from "react-router-dom"
 
 const Crewmate = (props) => {
     // remember to add link to edit the crewmate
     return (
-        <>
-        </>
+        <div className="Crewmate">
+            <Link to={'edit/' + props.id}>[edit]</Link>
+            <h2 className="name">{props.name}</h2>
+            <h3 className="speed">{props.speed}</h3>
+            <p className="color">{props.color}</p>
+        </div>
     )
 }
 
--- src/pages/ReadCrew.jsx
+++ src/pages/ReadCrew.jsx
@@ -3,9 +3,37 @@ import Crewmate from '../components/Crewmate'
 import { supabase } from '../client'
 
 const ReadCrew = () => {
+    const [crew, setCrew] = useState([])
+
+    useEffect(() => {
+        const fetchCrewmates = async () => {
+            const { data } = await supabase
+                .from("Crewmates")
+                .select()
+                .order("created_at", { ascending: true })
+            setCrew(data)
+        }
+        fetchCrewmates()
+    }, [])
+
     return (
-        <>
-        </>
+        <div className="readCrew">
+            {/* maps all the posts in the 'posts' variable */}
+            {
+                crew && crew.length > 0 ?
+                    [...crew]
+                        .sort((a, b) => a.id - b.id)
+                        .map((post, index) =>
+                            <Crewmate
+                                key={post.id}
+                                id={post.id}
+                                name={post.name}
+                                speed={post.speed}
+                                color={post.color}
+                            />
+                        ) : <h2>{"No Crewmates Yet"}</h2>
+            }
+        </div>
     )
 }
 
