251bad36859dbe555b7fcf9dfc1006f472456434
amiraunyasopon <amiraunyasopon@gmail.com>
2025-11-07 14:16:42 -0800

    created navbar, added useroutes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
-import { useState } from 'react'
 import './App.css'
-
+import { Link, useRoutes } from 'react-router-dom'
+import ReadCrew from "./pages/ReadCrew"
+import EditCrew from "./pages/EditCrew"
+import CreateCrew from "./pages/CreateCrew"
+import NavBar from "./components/NavBar"
 function App() {
+
+    let element = useRoutes([
+        {
+            path: "/",
+            element: <ReadCrew />
+        },
+        {
+            path: "edit/:id",
+            element: <EditCrew />
+        },
+        {
+            path: "/new",
+            element: <CreateCrew />
+        }
+    ]);
 
     return (
         <>
-            <h1>Test</h1>
-            <div>test2</div>
+            <div className="App">
+                <h1>test</h1>
+                <NavBar />
+                {element}
+            </div>
         </>
     )
 }
